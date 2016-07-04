3d51e5423c879cc451f083b475ebfbe9764e3882
Minori Awamura <minori.awamura@gmail.com>
2016-07-04 18:37:25 +0900

    Started @ 2016/7/4

---
+++ AWAGitTest/AWAGitTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AWAGitTest
+//
+//  Created by Minori Awamura on 2016/07/04.
+//  Copyright © 2016年 Minori Awamura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
